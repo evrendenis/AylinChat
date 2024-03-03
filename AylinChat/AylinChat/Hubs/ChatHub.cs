e5692e31d922150dddfbee6ff5411fecf708ec36
evrendenis <evrendenis08@hotmail.com>
2024-03-03 17:55:38 +0300

    Add project files.

---
+++ AylinChat/AylinChat/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace AylinChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string userName , string Message, DateTime date) 
+        => await Clients.All.SendAsync("ReciveMessage", userName,Message,date);
+
+        
+    }
+}
